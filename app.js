b85b805d429554a99a1f9c8c061536b1e3377810
lourdesnrdz <lulynavarrete@hotmail.com>
2019-10-24 12:37:09 -0500

    Examen Parcial 2 Aplicaciones Web

---
+++ app.js
@@ -0,0 +1,64 @@
+
+const express = require('express')
+const request = require('request')
+const met = require('./met.js')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+
+const matricula = 'A01039707'
+const nombre = 'Lourdes Navarrete Rodríguez'
+
+app.get('', function(req, res) {
+	res.send("Hola Mundo!")
+})
+
+app.get('/students/:id', function(req, res) {
+	if(req.params.id === matricula){
+		res.send({
+			id : matricula,
+			fullname : nombre,
+			nickname : "Luly",
+			age : 22
+		})
+	} else {
+		res.send({
+			ERROR : 'Matrícula Inválida'
+		})
+	}
+})
+
+app.get('/met', function(req, res) {
+	if (!req.query.search) {
+	    return res.send('ERROR: Debes enviar un objeto')
+	}
+
+	met.getObjectId(req.query.search, function(error, response) {
+		if(error){
+			return res.send({
+				error : error
+			})
+		}
+		met.getObjectInfo(response.objectId, response.search, function(error, response) {
+			if(error){
+				return res.send({
+					error : error
+				})
+			} 
+			res.send(response)
+		})
+	})
+
+})
+
+app.get('*', function(req, res) {
+  res.send({
+    error:'ERROR: Ruta no valida'
+  })
+})
+
+app.listen(port, function() {
+	console.log('up and at em')
+})
---
+++ met.js
@@ -0,0 +1,53 @@
+
+const request = require('request')
+
+const getObjectId = function (object, callback) {
+	const url = 'https://collectionapi.metmuseum.org/public/collection/v1/search?q=' + object
+
+	request({url, json:true}, function(error, response) {
+		if(error) {
+			callback("ERROR conexión fallida", null)
+		} else {
+			const data = response.body
+
+			if(data.total === 0){
+				callback("ERROR: No se oncontró objeto", null)
+			} else {
+				const info = {
+					objectId : data.objectIDs[0],
+					search: object
+				}
+
+				callback(null, info)
+			}
+		}
+	})
+}
+
+const getObjectInfo = function(objectId, search, callback) {
+	const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/' + objectId
+
+	request({url, json:true}, function(error, response) {
+		if(error) {
+			callback("ERROR conexión fallida", null)
+		} else {
+			const data = response.body
+
+			const info = {
+				searchTerm : search,
+				artist : data.constituents[0].name,
+				title : data.title,
+				year : data.objectEndDate,
+				technique : data.medium,
+				metUrl : data.objectURL
+			}
+
+				callback(null, info)
+		}
+	})
+}
+
+module.exports = {
+	getObjectId : getObjectId,
+	getObjectInfo : getObjectInfo
+}
